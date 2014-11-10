f1a91607dcccd07f9b0d17756cd8cbc71b6702aa
Vasyl Stokolosa <v.stokolosa@sysiq.com>
2014-11-10 18:51:28 +0200

    Start

---
+++ js/api.js
@@ -0,0 +1,12 @@
+/**
+ * Created by v.stokolosa on 11/10/14.
+ */
+
+/**
+ * Canvas
+ */
+var canvas = document.getElementById('snake-game'),
+    ctx = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 400;
+
